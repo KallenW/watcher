136497139413549c7dfe71cd527cf1ffa3b0e6b5
TENX-S <coldswind@pm.me>
2020-12-21 22:21:50 +0800

    Add examples and docs; Update README

--- examples/sync_dir.rs
+++ examples/sync_dir.rs
@@ -4,20 +4,21 @@ use std::thread;
 
 fn main() {
 
-    let watcher = Arc::new(Watcher::new(&std::env::args().skip(1).next().unwrap()).unwrap());
+    let target = std::env::args().skip(1).next().unwrap();
+    let watcher = Arc::new(Watcher::new(&target).unwrap());
 
     let update_watcher = Arc::clone(&watcher);
 
     thread::spawn(move || update_watcher.keep_sync_with_idle(None));
 
     let mut previous_length = watcher.get_snapshot().len();
-    println!("{}", previous_length);
+    println!("Number of entries at {}: {}", target, previous_length);
     loop {
         let current_length = watcher.get_snapshot().len();
         if current_length != previous_length {
-            println!("{}", current_length);
+            println!("Number of entries at {}: {}", target, current_length);
             previous_length = current_length;
-            println!("{:#?}", watcher.get_events());
+            println!("Events from the beginning: {:#?}", watcher.get_events());
         }
     }
 }
